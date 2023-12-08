87e4c63386574f486d6b4c3cc31630740022116b
Lizadmi <liza.dmitreva00@mail.ru>
2023-12-08 20:43:59 +0500

    first commit

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public Enemy EnemyPrefab;
+    public float TimeToSpawn;
+    public Transform[] Points;
+
+    private float timer;
+
+    void Start()
+    {
+        timer = TimeToSpawn;
+    }
+    
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if(timer <= 0)
+        {
+            Enemy enemy = Instantiate(EnemyPrefab, transform.position, Quaternion.identity);
+            enemy.Points = Points;
+            timer = TimeToSpawn;
+        }
+    }
+}
